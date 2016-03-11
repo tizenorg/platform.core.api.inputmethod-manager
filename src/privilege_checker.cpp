80d8eb298220c9158296fe6e380555dffc569c41
sungwook79.park <sungwook79.park@samsung.com>
2016-03-11 16:47:20 +0900

    cynara_privilege_check
    Change-Id: I4b90d777546f0d7c85a3362ac08cddfef12b888b
    Signed-off-by: sungwook79.park <sungwook79.park@samsung.com>

---
+++ src/privilege_checker.cpp
@@ -0,0 +1,64 @@
+#include "privilege_checker.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dlog.h>
+
+#include <cynara-client.h>
+#include <cynara-error.h>
+#include <cynara-creds-socket.h>
+#include <cynara-session.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "INPUTMETHOD_MANAGER"
+
+namespace
+{
+
+cynara *p_cynara = NULL;
+
+}
+
+bool
+inputmethod_cynara_initialize ()
+{
+    int ret = cynara_initialize (&p_cynara, NULL);
+    LOGW("[inputmethod_cynara_initialize]_check_privilege returned %d.", ret);
+    return ret == CYNARA_API_SUCCESS;
+}
+
+void
+inputmethod_cynara_finish ()
+{
+    if (p_cynara)
+        cynara_finish (p_cynara);
+
+    p_cynara = NULL;
+}
+
+bool
+checkPrivilege (const char *uid, const char *privilege)
+{
+    if (!p_cynara){
+        return false;
+    }
+
+    FILE *pFile = NULL;
+    char smackLabel[1024] = "/proc/self/attr/current";
+    pFile = fopen( "/proc/self/attr/current", "r" );
+    if( pFile != NULL )
+    {
+        fread(smackLabel, sizeof(smackLabel), 1, pFile);
+        fclose( pFile );
+    }
+    LOGW("smackLabel = %s", smackLabel);
+
+    int ret = cynara_check (p_cynara, smackLabel, NULL, uid, privilege);
+    LOGW("[checkPrivilege]_check_privilege returned %d.", ret);
+    if (ret != CYNARA_API_ACCESS_ALLOWED)
+        return false;
+    return true;
+}
