49988e54820ce086c070588793a119b323afddf8
sungwook79.park <sungwook79.park@samsung.com>
2016-03-14 15:26:13 +0900

    cynara_privilege_check
    Change-Id: Ie8f046aad66853f46da5b967a97fdee1d90b02e9
    Signed-off-by: sungwook79.park <sungwook79.park@samsung.com>

---
+++ include/privilege_checker_private.h
@@ -0,0 +1,10 @@
+#ifndef __PRIVILEGE_CHECKER_H
+#define __PRIVILEGE_CHECKER_H
+
+#define IME_MANAGER_PRIVILEGE "http://tizen.org/privilege/imemanager"
+
+bool inputmethod_cynara_initialize();
+void inputmethod_cynara_finish();
+bool check_privilege(const char *uid, const char *privilege);
+
+#endif //__PRIVILEGE_CHECKER_H
---
+++ src/privilege_checker.cpp
@@ -0,0 +1,68 @@
+#include "privilege_checker_private.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <dlog.h>
+#include <unistd.h>
+
+#include <cynara-client.h>
+#include <cynara-error.h>
+#include <cynara-creds-socket.h>
+#include <cynara-session.h>
+
+#ifdef LOG_TAG
+#undef LOG_TAG
+#endif
+#define LOG_TAG "INPUTMETHOD_MANAGER"
+
+namespace
+{
+cynara *p_cynara = NULL;
+}
+
+bool
+inputmethod_cynara_initialize()
+{
+    int ret = cynara_initialize(&p_cynara, NULL);
+    LOGD("[inputmethod_cynara_initialize]_check_privilege returned %d.", ret);
+    return ret == CYNARA_API_SUCCESS;
+}
+
+void
+inputmethod_cynara_finish()
+{
+    if (p_cynara)
+        cynara_finish(p_cynara);
+
+    p_cynara = NULL;
+}
+
+bool
+check_privilege(const char *uid, const char *privilege)
+{
+    if (!p_cynara) {
+        return false;
+    }
+
+    FILE *file = NULL;
+    char smackLabel[1024] = "/proc/self/attr/current";
+    file = fopen("/proc/self/attr/current", "r");
+    if (file != NULL) {
+        if (fread(smackLabel, 1, sizeof(smackLabel), file) <= 0)
+            LOGW("Error : fread");
+        }
+        fclose(file);
+    }
+
+    pid_t pid = getpid();
+    char *session = cynara_session_from_pid(pid);
+    int ret = cynara_check(p_cynara, smackLabel, session, uid, privilege);
+    LOGD("[check_privilege]_check_privilege returned %d.", ret);
+    if (session)
+        free(session);
+
+    if (ret != CYNARA_API_ACCESS_ALLOWED)
+        return false;
+    return true;
+}
